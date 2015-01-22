10e3bd57fb7af7ccc799644fc73af7899ea77697
Fabio Jun Takada Chino <fchino@opencs.com.br>
2015-01-22 17:07:28 -0200

    Test programs added.

---
+++ gtest/test/test.cpp
@@ -0,0 +1,13 @@
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+	int retval;
+
+	std::cout << "Running main() from gtest_main.cc\n";
+	testing::InitGoogleTest(&argc, argv);
+	retval = RUN_ALL_TESTS();
+
+	return retval;
+}
+
